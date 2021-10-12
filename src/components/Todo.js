5646aa3149272bf0bf9528a80c131562c88a92d0
MostafaMohammadi1989 <Mostafa.mohammadi.1989.10.26@gmail.com>
2021-10-11 23:35:08 -0700

    add btn

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,19 @@
 function Todo(props) {
-    return (
-        <div className='card'>
-        <h2>{props.text}</h2>
-        <div className='actions'>
-          <button className='btn'>Delet</button>
-        </div>
+  function deletHandler() {
+    alert("i try learning react for future :)");
+    console.log("I like to learn reac");
+    console.log(props.text);
+  }
+  return (
+    <div className="card">
+      <h2>{props.text}</h2>
+      <div className="actions">
+        <button className="btn" onClick={deletHandler}>
+          Delet
+        </button>
       </div>
-    )
- }
+    </div>
+  );
+}
 
 export default Todo;
